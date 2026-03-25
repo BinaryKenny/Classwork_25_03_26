5c84706fb5a60cba9c8fee480aa6a5ceaef656c4
BinaryKenny <khairullinvadim2006@gmail.com>
2026-03-25 15:24:50 +0300

    Test description

--- main.cpp
+++ main.cpp
@@ -18,19 +18,25 @@ bool testDefaultVectorIsEmpty(const char ** pname)
 
 int main()
 {
+  size_t failed = 0;
   using test_t = bool(*)(const char **);
-  test_t tests[] = {
-    testConstractAndDecstruct,
-    testDefaultVectorIsEmpty
+  using case_t = std::pair<test_t, const char *>;
+  case_t tests[] = {
+    {testConstractAndDecstruct, "Vector must be default constructable"},
+    {testDefaultVectorIsEmpty, "Default constructed Vector must be empty"}
   };
-  size_t count = sizeof(tests) / sizeof(test_t);
+  size_t count = sizeof(tests) / sizeof(case_t);
   for (size_t i = 0; i < count; i++)
   {
     const char * testName = nullptr;
-    bool r = tests[i](&testName);
+    bool r = tests[i].first(&testName);
     if (!r)
     {
-      std::cout << "Failed:" << " " << testName << "\n";
+      failed++;
+      std::cout << "[Fail] " << testName << "\n";
+      std::cout << "\t" << tests[i].second << "\n";
     }
   }
+  std::cout << "Summary:\n" << "\t" << (count - failed) << "passed\n";
+  std::cout << "\t" << count << "total\n";
 }
