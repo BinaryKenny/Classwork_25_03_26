d85e1b47c4332b7ca85762b6819213b6a5853d55
BinaryKenny <khairullinvadim2006@gmail.com>
2026-03-25 15:02:14 +0300

    Test for default constricting vector

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "pevector.hpp"
 #include <iostream>
+using iknk::Vector;
 
 bool test1()
 {
@@ -7,11 +8,18 @@ bool test1()
   return true;
 }
 
+bool test2()
+{
+  Vector<int> v;
+  return v.isEmpty();
+}
+
 int main()
 {
   using test_t = bool(*)();
   test_t tests[] = {
-    test1
+    test1,
+    test2
   };
   size_t count = sizeof(tests) / sizeof(test_t);
   for (size_t i = 0; i < count; i++)
--- pevector.hpp
+++ pevector.hpp
@@ -7,10 +7,22 @@ namespace iknk
   struct Vector {
     Vector();
     ~Vector();
+    bool isEmpty();
     private:
       T * data;
       size_t size, capacity;
   };
 }
 
+template<class T>
+iknk::Vector<T>():
+  data(nullptr),
+  size(0),
+  capacity(0)
+{}
+template <class T>
+iknk::Vector<T>::~Vector()
+{
+  delete [] data;
+}
 #endif
